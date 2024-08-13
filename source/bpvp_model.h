513a41dda09286a21b9491d0eb7be7d3a78ef7da
Pierre Guillot <guillotpierre6@gmail.com>
2024-08-13 15:07:48 +0200

    Add source

---
+++ source/bpvp_model.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstddef>
+#include <cstring>
+
+namespace Bpvp
+{
+    extern const void* model;
+    extern const size_t model_size;
+
+    static auto constexpr modelSampleRate = 22050;
+    static auto constexpr modelBlockSize = 43844;
+    static auto constexpr modelBlockDuration = static_cast<double>(modelBlockSize) / static_cast<double>(modelSampleRate);
+    static auto constexpr modelNumFrames = 172;
+    static auto constexpr modelNumNotes = 88;
+    static auto constexpr modelNoteOffset = 21;
+    static auto constexpr modelTensorSize = modelNumFrames * modelNumNotes;
+
+} // namespace Bpvp
