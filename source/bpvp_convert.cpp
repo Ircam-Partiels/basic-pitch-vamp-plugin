3a5635df84d5e09fcdaaee3a0c558a291ede5fd1
Pierre Guillot <guillotpierre6@gmail.com>
2024-08-22 16:10:06 +0200

    Improve note conversion and add model overlapping

--- source/bpvp_convert.cpp
+++ source/bpvp_convert.cpp
@@ -18,93 +18,107 @@ namespace Bpvp
 
     static constexpr double frameToSeconds(auto frame)
     {
-        return static_cast<double>(frame) / static_cast<double>(modelNumFrames) * modelBlockDuration;
+        return static_cast<double>(frame) / static_cast<double>(modelNumFrames) * modelBlockDuration + 0.1751664994f;
     }
 
     static constexpr long secondsToFrame(auto seconds)
     {
-        return static_cast<long>(std::round(seconds / modelBlockDuration * static_cast<double>(modelNumFrames)));
+        return static_cast<long>(std::ceil((seconds) / modelBlockDuration * static_cast<double>(modelNumFrames)));
     }
 
-    static std::vector<float> getInferredOnsets(std::vector<float> const& onsets, std::vector<float> const& frames, size_t numFrames, size_t numNotes, size_t numDiff = 2)
+    static std::vector<std::array<float, modelNumNotes>> getInferredOnsets(std::vector<std::array<float, modelNumNotes>> const& onsets, std::vector<std::array<float, modelNumNotes>> const& frames, size_t numDiff = 2)
     {
-        std::vector<float> notesDiff(numFrames * numNotes, 1.0f);
+        std::vector<std::array<float, modelNumNotes>> notesDiff;
+        notesDiff.resize(frames.size());
+        for(auto& notes : notesDiff)
+        {
+            std::fill(notes.begin(), notes.end(), 1.0f);
+        }
+
         auto maxDiff = 0.0f;
         auto maxOnset = 0.0f;
         for(size_t diff = 1; diff <= numDiff; ++diff)
         {
-            for(size_t frame = 0; frame < numFrames; ++frame)
+            for(size_t frame = 0; frame < frames.size(); ++frame)
             {
-                for(size_t note = 0; note < numNotes; ++note)
+                for(size_t note = 0; note < modelNumNotes; ++note)
                 {
-                    auto const currentEnergy = frames.at(frame * numNotes + note);
-                    auto const previousEnergy = (frame >= diff) ? frames.at((frame - diff) * numNotes + note) : 0.0f;
+                    auto const currentEnergy = frames.at(frame).at(note);
+                    auto const previousEnergy = (frame >= diff) ? frames.at(frame - diff).at(note) : 0.0f;
                     auto const diffEnergy = std::max(currentEnergy - previousEnergy, 0.0f);
-                    notesDiff[frame * numNotes + note] = std::min((frame >= numDiff) ? diffEnergy : 0.0f, notesDiff.at(frame * numNotes + note));
+                    notesDiff[frame][note] = std::min((frame >= numDiff) ? diffEnergy : 0.0f, notesDiff.at(frame).at(note));
 
-                    maxOnset = std::max(onsets.at(frame * numNotes + note), maxOnset);
-                    maxDiff = std::max(notesDiff.at(frame * numNotes + note), maxDiff);
+                    maxOnset = std::max(onsets.at(frame).at(note), maxOnset);
+                    maxDiff = std::max(notesDiff.at(frame).at(note), maxDiff);
                 }
             }
         }
 
         auto const ratio = maxDiff >= 0.0f ? maxOnset / maxDiff : 0.0f;
-        std::transform(onsets.cbegin(), onsets.cend(), notesDiff.cbegin(), notesDiff.begin(), [&](auto const& lhs, auto const& rhs)
-                       {
-                           return std::max(lhs, rhs * ratio);
-                       });
+        for(size_t frame = 0; frame < frames.size(); ++frame)
+        {
+            std::transform(onsets.at(frame).cbegin(), onsets.at(frame).cend(), notesDiff.at(frame).cbegin(), notesDiff[frame].begin(), [&](auto const& lhs, auto const& rhs)
+                           {
+                               return std::max(lhs, rhs * ratio);
+                           });
+        }
         return notesDiff;
     }
 
-    std::vector<Note> getNotes(std::vector<float> const& currentFrames, std::vector<float> const& currentOnsets, size_t numFrames, size_t numNotes, bool inferOnsets, float frameEnergyThreshold, float onsetEnergyThreshold, double minNoteDuration, long maxFramesBelowThreshold, std::optional<float> const maxFreq, std::optional<float> const minFreq, bool melodiaTrick)
+    std::vector<Note> getNotes(std::vector<std::array<float, modelNumNotes>> const& currentFrames, std::vector<std::array<float, modelNumNotes>> const& currentOnsets, bool inferOnsets, size_t voiceIndex, float frameEnergyThreshold, float onsetEnergyThreshold, double minNoteDuration, long maxFramesBelowThreshold, std::optional<float> const minFreq, std::optional<float> const maxFreq, bool melodiaTrick)
     {
         std::vector<Note> notes;
-        auto onsets = inferOnsets ? getInferredOnsets(currentOnsets, currentFrames, numFrames, numNotes) : currentOnsets;
+        auto onsets = inferOnsets ? getInferredOnsets(currentOnsets, currentFrames) : currentOnsets;
         auto frames = currentFrames;
 
+        auto const numFrames = frames.size();
         auto const minNoteLength = secondsToFrame(minNoteDuration);
         auto const lastFrameIndex = numFrames - 1;
-        auto const maxNoteIndex = maxFreq.has_value() ? static_cast<size_t>(std::round(hertzToMidi(maxFreq.value())) - modelNoteOffset) : numNotes;
-        auto const minNoteIndex = minFreq.has_value() ? static_cast<size_t>(std::round(hertzToMidi(minFreq.value())) - modelNoteOffset) : size_t(0);
+        auto const maxNoteIndex = std::clamp(maxFreq.has_value() ? static_cast<size_t>(std::round(hertzToMidi(maxFreq.value())) - modelNoteOffset) : modelNumNotes, size_t(0), size_t(modelNumNotes));
+        auto const minNoteIndex = std::clamp(minFreq.has_value() ? static_cast<size_t>(std::round(hertzToMidi(minFreq.value())) - modelNoteOffset) : size_t(0), size_t(0), size_t(modelNumNotes));
 
         for(long frameStartIndex = static_cast<long>(lastFrameIndex) - 1; frameStartIndex >= 0; --frameStartIndex)
         {
+            auto const fsi = static_cast<size_t>(frameStartIndex);
             for(long noteIndex = static_cast<long>(maxNoteIndex) - 1; noteIndex >= static_cast<long>(minNoteIndex); noteIndex--)
             {
-                auto const currentOnset = onsets.at(static_cast<size_t>(frameStartIndex) * numNotes + static_cast<size_t>(noteIndex));
-                auto const previousOnset = (frameStartIndex <= 0) ? currentOnset : onsets.at(static_cast<size_t>(frameStartIndex - 1) * numNotes + static_cast<size_t>(noteIndex));
-                auto const nextOnset = (frameStartIndex >= static_cast<long>(lastFrameIndex)) ? currentOnset : onsets.at(static_cast<size_t>(frameStartIndex + 1) * numNotes + static_cast<size_t>(noteIndex));
+                auto const ni = static_cast<size_t>(noteIndex);
+                auto const currentOnset = onsets.at(fsi).at(ni);
+                auto const previousOnset = (fsi <= 0) ? currentOnset : onsets.at(fsi - 1).at(ni);
+                auto const nextOnset = (fsi >= lastFrameIndex) ? currentOnset : onsets.at(fsi + 1).at(ni);
+
                 if(currentOnset >= onsetEnergyThreshold && currentOnset >= previousOnset && currentOnset >= nextOnset)
                 {
-                    auto frameEndIndex = frameStartIndex + 1;
+                    auto fei = fsi + 1;
                     auto accumulatedFrames = 0;
-                    while(frameEndIndex < lastFrameIndex && accumulatedFrames < maxFramesBelowThreshold)
+                    while(fei < lastFrameIndex && accumulatedFrames < maxFramesBelowThreshold)
                     {
-                        auto const energy = frames.at(static_cast<size_t>(frameEndIndex) * numNotes + static_cast<size_t>(noteIndex));
+                        auto const energy = frames.at(fei).at(ni);
                         accumulatedFrames = energy < frameEnergyThreshold ? accumulatedFrames + 1 : 0;
-                        ++frameEndIndex;
+                        ++fei;
                     }
-                    frameEndIndex -= accumulatedFrames;
-                    auto const frameDuration = frameEndIndex - frameStartIndex;
+                    fei -= accumulatedFrames;
+                    auto const frameDuration = fei - fsi;
+
                     if(frameDuration > minNoteLength)
                     {
                         auto amplitude = 0.0;
-                        for(auto f = frameStartIndex; f < frameEndIndex; f++)
+                        for(auto cf = fsi; cf < fei; cf++)
                         {
-                            auto const index = static_cast<size_t>(f) * numNotes + static_cast<size_t>(noteIndex);
-                            frames[index] = 0.0f;
-                            if(noteIndex < numNotes - 1)
+                            auto& cframe = frames[cf];
+                            cframe[ni] = 0.0f;
+                            if(ni < modelNumNotes - 1)
                             {
-                                frames[index + 1] = 0.0f;
+                                cframe[ni + 1] = 0.0f;
                             }
-                            if(noteIndex > 0)
+                            if(ni > 0)
                             {
-                                frames[index - 1] = 0.0f;
+                                cframe[ni - 1] = 0.0f;
                             }
-                            amplitude += static_cast<double>(currentFrames.at(index));
+                            amplitude += static_cast<double>(currentFrames.at(cf).at(ni));
                         }
                         amplitude /= static_cast<double>(frameDuration);
-                        notes.push_back({frameToSeconds(frameStartIndex), frameToSeconds(frameEndIndex), midiToHertz(noteIndex + modelNoteOffset), static_cast<float>(amplitude)});
+                        notes.push_back({frameToSeconds(fsi), frameToSeconds(fei), midiToHertz(ni + modelNoteOffset), static_cast<float>(amplitude)});
                     }
                 }
             }
@@ -114,105 +128,127 @@ namespace Bpvp
         {
             for(long frameIndex = static_cast<long>(lastFrameIndex) - 1; frameIndex >= 0; --frameIndex)
             {
+                auto const fi = static_cast<size_t>(frameIndex);
                 for(long noteIndex = static_cast<long>(maxNoteIndex) - 1; noteIndex >= static_cast<long>(minNoteIndex); noteIndex--)
                 {
-                    auto const index = static_cast<size_t>(frameIndex) * numNotes + noteIndex;
-                    auto const energy = frames.at(index);
+                    auto const ni = static_cast<size_t>(noteIndex);
+                    auto const energy = frames.at(fi).at(ni);
                     if(energy > frameEnergyThreshold)
                     {
-                        frames[index] = 0.0f;
-                        auto frameEndIndex = frameIndex + 1;
+                        frames[fi][ni] = 0.0f;
+                        auto fei = frameIndex + 1;
                         {
                             auto accumulatedFrames = 0;
-                            while(frameEndIndex < lastFrameIndex && accumulatedFrames < maxFramesBelowThreshold)
+                            while(fei < lastFrameIndex && accumulatedFrames < maxFramesBelowThreshold)
                             {
-                                auto const cenergy = frames.at(static_cast<size_t>(frameEndIndex) * numNotes + static_cast<size_t>(noteIndex));
-                                accumulatedFrames = cenergy < frameEnergyThreshold ? accumulatedFrames + 1 : 0;
-
-                                frames[static_cast<size_t>(frameEndIndex) * numNotes + static_cast<size_t>(noteIndex)] = 0.0f;
-                                if(noteIndex < numNotes - 1)
+                                auto& cframe = frames[fei];
+                                accumulatedFrames = cframe.at(ni) < frameEnergyThreshold ? accumulatedFrames + 1 : 0;
+                                cframe[ni] = 0.0f;
+                                if(noteIndex < modelNumNotes - 1)
                                 {
-                                    frames[static_cast<size_t>(frameEndIndex) * numNotes + static_cast<size_t>(noteIndex) + 1] = 0.0f;
+                                    cframe[ni + 1] = 0.0f;
                                 }
                                 if(noteIndex > 0)
                                 {
-                                    frames[static_cast<size_t>(frameEndIndex) * numNotes + static_cast<size_t>(noteIndex) - 1] = 0.0f;
+                                    cframe[ni - 1] = 0.0f;
                                 }
-                                ++frameEndIndex;
+                                ++fei;
                             }
-                            frameEndIndex -= (accumulatedFrames + 1);
+                            fei -= (accumulatedFrames + 1);
                         }
 
-                        auto frameStartIndex = frameIndex - 1;
+                        auto fsi = frameIndex - 1;
                         {
                             auto accumulatedFrames = 0;
-                            while(frameStartIndex > 0 && accumulatedFrames < maxFramesBelowThreshold)
+                            while(fsi > 0 && accumulatedFrames < maxFramesBelowThreshold)
                             {
-                                auto const cenergy = frames.at(static_cast<size_t>(frameStartIndex) * numNotes + static_cast<size_t>(noteIndex));
-                                accumulatedFrames = cenergy < frameEnergyThreshold ? accumulatedFrames + 1 : 0;
-
-                                frames[static_cast<size_t>(frameStartIndex) * numNotes + static_cast<size_t>(noteIndex)] = 0.0f;
-                                if(noteIndex < numNotes - 1)
+                                auto& cframe = frames[fsi];
+                                accumulatedFrames = cframe.at(ni) < frameEnergyThreshold ? accumulatedFrames + 1 : 0;
+                                cframe[ni] = 0.0f;
+                                if(noteIndex < modelNumNotes - 1)
                                 {
-                                    frames[static_cast<size_t>(frameStartIndex) * numNotes + static_cast<size_t>(noteIndex) + 1] = 0.0f;
+                                    cframe[ni + 1] = 0.0f;
                                 }
                                 if(noteIndex > 0)
                                 {
-                                    frames[static_cast<size_t>(frameStartIndex) * numNotes + static_cast<size_t>(noteIndex) - 1] = 0.0f;
+                                    cframe[ni - 1] = 0.0f;
                                 }
-                                --frameStartIndex;
+                                --fsi;
                             }
 
-                            frameStartIndex += (accumulatedFrames + 1);
+                            fsi += (accumulatedFrames + 1);
                         }
 
-                        assert(frameStartIndex >= 0);
-                        assert(frameEndIndex < numFrames);
+                        assert(fsi >= 0);
+                        assert(fei < numFrames);
 
-                        auto const frameDuration = frameEndIndex - frameStartIndex;
+                        auto const frameDuration = fei - fsi;
                         if(frameDuration > minNoteLength)
                         {
                             auto amplitude = 0.0;
-                            for(auto f = frameStartIndex; f < frameEndIndex; f++)
+                            for(auto cf = fsi; cf < fei; cf++)
                             {
-                                auto const cindex = static_cast<size_t>(f) * numNotes + static_cast<size_t>(noteIndex);
-                                amplitude += static_cast<double>(currentFrames.at(cindex));
+                                amplitude += static_cast<double>(currentFrames.at(cf).at(ni));
                             }
                             amplitude /= static_cast<double>(frameDuration);
-                            notes.push_back({frameToSeconds(frameStartIndex), frameToSeconds(frameEndIndex), midiToHertz(noteIndex + modelNoteOffset), static_cast<float>(amplitude)});
+                            notes.push_back({frameToSeconds(fsi), frameToSeconds(fei), midiToHertz(ni + modelNoteOffset), static_cast<float>(amplitude)});
                         }
                     }
                 }
             }
         }
-        std::sort(notes.begin(), notes.end(), [](auto const& lhs, auto const& rhs)
-                  {
-                      return lhs.start < rhs.start || (std::abs(lhs.start - rhs.start) < std::numeric_limits<double>::epsilon() && lhs.end < rhs.end);
-                  });
+
+        auto const noteCmp = [](auto const& lhs, auto const& rhs)
+        {
+            return lhs.start < rhs.start || (lhs.start <= rhs.start && lhs.pitch < rhs.pitch);
+        };
 
         if(!notes.empty())
         {
-            auto next = std::next(notes.begin());
-            while(next != notes.end())
+            std::sort(notes.begin(), notes.end(), noteCmp);
+            for(auto it = notes.begin(); it < notes.end(); ++it)
             {
-                auto it = std::prev(next);
-                if(next->start < it->end)
+                auto next = std::next(it);
+                while(next != notes.end() && next->start < it->end)
                 {
-                    if(next->pitch < it->pitch)
+                    if(std::abs(next->pitch - it->pitch) < std::numeric_limits<float>::epsilon())
                     {
-                        next = notes.erase(it);
+                        it->end = std::max(it->end, next->end);
+                        next = notes.erase(next);
                     }
                     else
                     {
-                        next = notes.erase(next);
+                        next = std::next(next);
                     }
                 }
-                else
-                {
-                    next = std::next(next);
-                }
             }
+
+            //            for(size_t voice = 0; voice <= voiceIndex; ++voice)
+            //            {
+            //                std::vector<Note> remainings;
+            //                for(auto it = notes.begin(); it < notes.end(); ++it)
+            //                {
+            //                    auto next = std::next(it);
+            //                    while(next != notes.end() && next->start < it->end)
+            //                    {
+            //                        if(next->pitch > it->pitch)
+            //                        {
+            //                            remainings.push_back(*next);
+            //                            next = notes.erase(next);
+            //                        }
+            //                        else
+            //                        {
+            //                            next = std::next(next);
+            //                        }
+            //                    }
+            //                }
+            //                if(voice < voiceIndex)
+            //                {
+            //                    notes = remainings;
+            //                }
+            //            }
         }
+
         return notes;
     }
 } // namespace Bpvp
--- source/bpvp_model.h
+++ source/bpvp_model.h
@@ -8,8 +8,10 @@ namespace Bpvp
     extern const size_t model_size;
 
     static auto constexpr modelSampleRate = 22050;
+    static auto constexpr modelFFTHope = 256;
     static auto constexpr modelBlockSize = 43844;
-    static auto constexpr modelBlockDuration = static_cast<double>(modelBlockSize) / static_cast<double>(modelSampleRate);
+    static auto constexpr modelBlockPadding = 7680;
+    static auto constexpr modelBlockDuration = static_cast<double>(modelBlockSize - modelBlockPadding) / static_cast<double>(modelSampleRate);
     static auto constexpr modelNumFrames = 172;
     static auto constexpr modelNumNotes = 88;
     static auto constexpr modelNoteOffset = 21;
